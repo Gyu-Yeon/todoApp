7cda0c422fdf45e9358fbe6efbd729a429f69ce6
Gyu-Yeon <idsopy@nate.com>
2022-02-05 01:55:11 +0900

    almost done

--- README.md
+++ README.md
@@ -2,56 +2,56 @@
 
 # Today's lessons.
 
-
 ## 1. How to make server in Node.js
+
 - write npm install express in your terminal( ` + ctrl)
 - make a new js file name server.js and write this on the top of file.
-<br> const express = require("express");
-<br> const app = express();
+  <br> const express = require("express");
+  <br> const app = express();
 - and write app.listen to connect to port you want.
-<br> ex)
-<br> app.listen("8080", function () {
-<br>      console.log("listening on 8080");
-<br>    });
+  <br> ex)
+  <br> app.listen("8080", function () {
+  <br> console.log("listening on 8080");
+  <br> });
 - write node server.js in your terminal.
 - open any browser and go to localhost:8080
-- if the page says cannot get / you are ready to start. 
+- if the page says cannot get / you are ready to start.
 
-2022.01.15
+  2022.01.15
 
 ## 2. How to send delete request to server from HTML.
+
 - first you need jquery cdn or jqeury script file.
 - write the ajax method between script tag.
-<br> ex)
-<br> < script >
-<br> $.ajax({
-<br>  method: "DELET",
-<br>  url: '/delete',
-<br>  data; { _id: e.target.dataset.id}    
-<br> }).done(function() {
-<br>  $( this ).addClass( "done" );
-<br> });
-<br> < script >
+  <br> ex)
+  <br> < script >
+  <br> $.ajax({
+  <br> method: "DELET",
+  <br> url: '/delete',
+  <br> data; { \_id: e.target.dataset.id}  
+  <br> }).done(function() {
+  <br> $( this ).addClass( "done" );
+  <br> });
+  <br> < script >
 - for data write down what you want to send to server with ajax request.
 - for url write down the path that this request will run.
 - for method choose the kind of request you want to send.
 
-
- 
-2022.01.16
+  2022.01.16
 
 ## 3. How to make Login function.
+
 - npm install passport passport-local express-session
 - const passport = require('passport');
-<br> const LocalStrategy = require('passport-local').Strategy;
-<br> const session = require('express-session');
-<br> app.use(session({secret : 'secretCode', resave : true, saveUninitialized: false}));
-<br> app.use(passport.initialize());
-<br> app.use(passport.session()); 
+  <br> const LocalStrategy = require('passport-local').Strategy;
+  <br> const session = require('express-session');
+  <br> app.use(session({secret : 'secretCode', resave : true, saveUninitialized: false}));
+  <br> app.use(passport.initialize());
+  <br> app.use(passport.session());
 - app.get('/login', function(req, res){
-<br>  req.render('login.ejs')
-<br>});
+  <br> req.render('login.ejs')
+  <br>});
 
+  2022.01.30
 
-2022.01.30
-
+## 4.
--- server.js
+++ server.js
@@ -2,10 +2,53 @@ const express = require("express");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
 const methodOverride = require("method-override");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
+
+app.use(
+  session({ secret: "비밀코드", resave: true, saveUninitialized: false })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.use("/public", express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "id",
+      passwordField: "pw",
+      session: true,
+      passReqToCallback: false,
+    },
+    function (whatId, whatPw, done) {
+      console.log(whatId, whatPw);
+      db.collection("login").findOne({ id: whatId }, function (err, result) {
+        if (err) return done(err);
+
+        if (!result)
+          return done(null, false, { message: "Check your ID again" });
+        if (whatPw == result.pw) {
+          return done(null, result);
+        } else {
+          return done(null, false, { message: "Wrong" });
+        }
+      });
+    }
+  )
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  db.collection("login").findOne({ id: id }, function (err, result) {
+    done(null, { result });
+  });
+});
 
 let db;
 MongoClient.connect(
@@ -21,12 +64,27 @@ MongoClient.connect(
   }
 );
 
-app.get("/", function (req, res) {
+function logined(req, res, next) {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+}
+
+app.get("/", logined, function (req, res) {
+  console.log(req.user);
+  let user = req.user;
   db.collection("post")
-    .find()
+    .find({ writer: "1" })
     .toArray(function (err, result) {
-      res.render("home.ejs", { data: result });
+      console.log(result);
+      res.render("home.ejs", { data: [result, user] });
     });
+});
+
+app.get("/login", function (req, res) {
+  res.render("login.ejs");
 });
 
 app.get("/lists", function (req, res) {
@@ -70,6 +128,7 @@ app.post("/add", function (req, res) {
           title: req.body.title,
           date: req.body.date,
           detail: req.body.detail,
+          writer: req.body.writer,
         },
         function (err, result) {
           db.collection("counter").updateOne(
@@ -85,6 +144,40 @@ app.post("/add", function (req, res) {
       );
     }
   );
+});
+
+app.post(
+  "/login",
+  passport.authenticate("local", { failureRedirect: "/login" }),
+  function (req, res) {
+    res.redirect("/");
+  }
+);
+
+app.post("/register", function (req, res) {
+  db.collection("counter").findOne({ name: "유저" }, function (err, result) {
+    console.log(result.to);
+    let userCode = result.totalUser;
+    db.collection("login").insertOne(
+      {
+        _id: userCode + 1,
+        userName: req.body.userName,
+        id: req.body.id,
+        pw: req.body.pw,
+      },
+      function (err, result) {
+        db.collection("counter").updateOne(
+          { name: "유저" },
+          { $inc: { totalUser: 1 } },
+          function (err, result) {
+            console.log("new Guy");
+          }
+        );
+      }
+    );
+    res.redirect("/login");
+  });
+  console.log(req.body);
 });
 
 app.put("/edit", function (req, res) {
